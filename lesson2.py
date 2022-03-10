220669ecf9a8b5dfec0c98bbc16f7b8f4d9e5684
Kurseev Maxim <mkurseev@yandex.ru>
2022-03-11 00:24:39 +0300

    completed

--- lesson2.py
+++ lesson2.py
@@ -1,4 +1,27 @@
-import pandas as pd
 
-df = pd.read_excel('Rates_2021_07_15.xlsx')
-df.head()
+# вычисляемые property
+
+class Square:
+
+    def __init__(self, s):
+        self.__side = s
+        self.__area = None
+
+
+    @property
+    def side(self):
+        return self.__side
+
+
+    @side.setter
+    def side(self, value):
+        self.__side = value
+        self.__area = None
+
+
+    @property
+    def area(self):
+        if self.__area is None:
+            print('calculate area')
+            self.__area = self.side ** 2
+        return self.__area
