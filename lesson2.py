224056654a7c3278e710804cc14d9022081ba821
Kurseev Maxim <mkurseev@yandex.ru>
2022-03-11 14:03:54 +0300

    practice time is done

--- lesson2.py
+++ lesson2.py
@@ -1,23 +1,20 @@
-
 # вычисляемые property
-
+# мы пришли к тому, что сделали и side и area атрибутами настраиваемыми, вместо того
+# чтобы это была функция просто
 class Square:
 
     def __init__(self, s):
         self.__side = s
         self.__area = None
 
-
     @property
     def side(self):
         return self.__side
-
 
     @side.setter
     def side(self, value):
         self.__side = value
         self.__area = None
-
 
     @property
     def area(self):
@@ -25,3 +22,54 @@ class Square:
             print('calculate area')
             self.__area = self.side ** 2
         return self.__area
+#_____________________________________________________________________
+
+# classmethod and staticmethod
+
+# Проблема : функцию hello мы не можем вызвать элементом класса, но можем классом
+# а функцию instance_hello не можем вызвать классом, но можем экземпляром класса
+class Example:
+    def hello():
+        print('Hello')
+
+    def instance_hello(self):
+        print(f'Hello {self}')
+
+    @staticmethod   # не привязывается ни к экземпляру, ни к классу
+    def static_hello():
+        print('static hello')
+
+    # нужен, когда хотим сделать обработку не над экземпляром,
+    # а над классом
+    # Внутри cls нам уже доступен класс со всеми методами и атрибутами
+    @classmethod
+    def class_hello(cls):
+        print(f'Hello {cls}')
+#_____________________________________________________________________
+
+# 2.9 from stepic
+class Robot:
+    population = 0
+
+    def __init__(self,name):
+        self.name = name
+        print(f'Робот {self.name} был создан')
+        Robot.population += 1
+
+    def destroy(self):
+        print(f'Робот {self.name} был уничтожен')
+        Robot.population -= 1
+
+    def say_hello(self):
+        print(f'Робот {self.name} приветствует тебя, особь человеческого рода')
+
+    @classmethod
+    def how_many(cls):
+        print(f'{cls.population}, вот сколько нас еще осталось')
+
+r2 = Robot("R2-D2") # печатает "Робот R2-D2 был создан"
+r2.say_hello() # печатает "Робот R2-D2 приветствует тебя, особь человеческого рода"
+Robot.how_many() # печатает "1, вот сколько нас еще осталось"
+r2.destroy() # печатает "Робот R2-D2 был уничтожен"
+#_____________________________________________________________________
+
---
+++ lesson3.py
@@ -0,0 +1,80 @@
+# Пространство имен класса
+
+class DepartmentIT:
+    PYTHON_DEV = 4
+    GO_DEV = 3
+    REACT_DEV = 2
+
+    # как обращаться к переменным класса.
+    # 1 способ - через self
+    def info(self):
+        print(self.PYTHON_DEV, self.GO_DEV, self.REACT_DEV)
+
+    #  2 способ - через сам класс
+    def info2(self):
+        print(DepartmentIT.PYTHON_DEV, DepartmentIT.GO_DEV, DepartmentIT.REACT_DEV)
+
+    #  3 способ - через property (getter), это уже свойство, а не метод (функция)
+    @property
+    def info_prop(self):
+        print(self.PYTHON_DEV, self.GO_DEV, self.REACT_DEV)
+
+    #  4 способ - через classmethod  ( на вход сам класс)
+    @classmethod
+    def info_class(cls):
+        print(cls.PYTHON_DEV, cls.GO_DEV, cls.REACT_DEV)
+
+    #  5 способ - через staticmethod  ( на вход ничего не принимается)
+    @staticmethod
+    def info_stat():
+        print(DepartmentIT.PYTHON_DEV, DepartmentIT.GO_DEV, DepartmentIT.REACT_DEV)
+
+
+# _____________________________________________________________________
+# Practice time  ( all topics covered above)
+from string import digits
+
+
+class User:
+
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+        self.__secret = 'sdfsafgsdcvb'
+
+    @property
+    def secret(self):
+        s = input('Password: ')
+        if s == self.password:
+            print(self.__secret)
+        else:
+            raise ValueError('Wrong password')
+
+    @property
+    def password(self):
+        print('getter called')
+        return self.__password
+
+    @staticmethod
+    def is_include_number(password):
+        for digit in digits:
+            if digit in password:
+                return True
+        return False
+
+    @password.setter
+    def password(self, new_password):
+        print('setter called')
+        if not isinstance(new_password, str):
+            raise TypeError("Password mast be a string")
+        if len(new_password) < 4:
+            raise ValueError('Password is too short, 4 is the minimum length')
+        if len(new_password) > 12:
+            raise ValueError('Password is too long, 12 is the maximum length')
+        if not User.is_include_number(new_password):
+            raise ValueError('Password should include at least one number')
+        self.__password = new_password
+
+
+x = User('aaa', '123rtgr')
+x.secret
