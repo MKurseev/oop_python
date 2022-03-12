e0334a709c8b302ab0164b0a6873ab4acea8c327
Kurseev Maxim <mkurseev@yandex.ru>
2022-03-13 00:17:45 +0300

    third module 3.1 completed

--- lesson3_1.py
+++ lesson3_1.py
@@ -1,1 +1,12 @@
 # magic methods __str__ and __repr__
+
+class Lion:
+    def __init__(self, name):
+        self.name = name
+
+    # как разработчики будут видеть этот объект класса
+    def __repr__(self):
+        return f'The object Lion - {self.name}'
+
+    def __str__(self):
+        return f'Lion - {self.name}'
--- tasks_module_3.py
+++ tasks_module_3.py
@@ -1,1 +1,31 @@
 # 3.1 task 1
+class Person:
+    def __init__(self, name, surname, gender='male'):
+        self.name = name
+        self.surname = surname
+        if gender not in ['male', 'female']:
+            print('Не знаю, что вы имели ввиду? Пусть это будет мальчик!')
+            self.gender = 'male'
+        else:
+            self.gender = gender
+
+    def __str__(self):
+        return f'Гражданин {self.surname} {self.name}' if self.gender == 'male' \
+            else f'Гражданка {self.surname} {self.name}'
+
+
+# ____________________________________________________________________________________________
+# 3.1 task 2
+
+class Vector:
+
+    def __init__(self, *args):
+        self.values = [str(i) for i in args if isinstance(i, int)]
+        self.values.sort()
+
+    def __str__(self):
+        return f"Вектор({', '.join(self.values)})" if len(self.values) != 0 \
+            else f'Пустой вектор'
+
+# ____________________________________________________________________________________________
+# 3.3
