7a5d8dc9c147cf2fa6c7075154b38c1e6b01f552
Kurseev Maxim <mkurseev@yandex.ru>
2022-03-12 23:34:18 +0300

    first and second modules completed

--- lesson3.py
+++ lesson3.py
@@ -76,8 +76,87 @@ class User:
         self.__password = new_password
 
 
-x = User('aaa', '123rtgr')
-x.secret
+# x = User('aaa', '123rtgr')
+# x.secret
+
 
 # _____________________________________________________________________
 # 2.11 from Stepic
+import string
+
+
+class Registration:
+
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+
+    @property
+    def login(self):
+        return self.__login
+
+    @property
+    def password(self):
+        return self.__password
+
+    @login.setter
+    def login(self, new_login):
+        if not Registration.is_included_sobaka(new_login):
+            raise ValueError("Login must include at least one '@'")
+        if not Registration.is_included_doth(new_login):
+            raise ValueError("Login must include at least one '.'")
+        self.__login = new_login
+
+    @staticmethod
+    def is_included_sobaka(login):
+        return True if '@' in login else False
+
+    @staticmethod
+    def is_included_doth(login):
+        return True if '.' in login else False
+
+    @staticmethod
+    def is_include_digit(password):
+        for digit in password:
+            if digit in string.digits:
+                return True
+        return False
+
+    @staticmethod
+    def is_include_all_register(password):
+        counter, flag2 = 0, False
+        for l in password:
+            if l.isupper():
+                counter += 1
+            if not l.isupper():
+                flag2 = True
+        return True if counter > 1 and flag2 else False
+
+    @staticmethod
+    def is_include_only_latin(password):
+        for l in password:
+            if l not in string.ascii_letters:
+                if l not in string.digits:
+                    return False
+        return True
+
+    @password.setter
+    def password(self, new_password):
+        if not isinstance(new_password, str):
+            raise TypeError('Пароль должен быть строкой')
+        if len(new_password) <= 4 or len(new_password) >= 12:
+            raise ValueError('Пароль должен быть длиннее 4 и меньше 12 символов')
+        if not Registration.is_include_digit(new_password):
+            raise ValueError('Пароль должен содержать хотя бы одну цифру')
+        if not Registration.is_include_all_register(new_password):
+            raise ValueError('Пароль должен содержать хотя бы 2 заглавные буквы')
+        if not Registration.is_include_only_latin(new_password):
+            raise ValueError('Пароль должен содержать только латинский алфавит')
+        self.__password = new_password
+
+
+me = Registration('mkurseev@yandex.ru', 'KUrseev1')
+me.login = 'nkurseeva@yand.ry'
+me.password = 'KKAHKJjjjn1'
+print(me.login)
+print(me.password)
