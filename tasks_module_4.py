1636fe57677f90b5e5f56f4d271c94a43b2d85fa
Kurseev Maxim <mkurseev@yandex.ru>
2022-03-17 14:04:25 +0300

    course has completed

--- tasks_module_4.py
+++ tasks_module_4.py
@@ -140,3 +140,38 @@ class SweetTooth(Initialization):
 
     def __gt__(self, other):
         return self.check(lambda x, y: x > y, self.capacity, other)
+
+
+# ____________________________________________________________________________________________
+# 5.1
+
+class Wallet:
+
+    def __init__(self, currency, balance):
+        if not isinstance(currency, str):
+            raise TypeError('Неверный тип валюты')
+        if len(currency) != 3:
+            raise NameError('Неверная длина названия валюты')
+        if not currency.isupper():
+            raise ValueError('Название должно состоять только из заглавных букв')
+        self.currency = currency
+        self.balance = balance
+
+    def __eq__(self, other):
+        if not isinstance(other, Wallet):
+            raise TypeError(f'Wallet не поддерживает сравнение с {other}')
+        if self.currency != other.currency:
+            raise ValueError("Нельзя сравнить разные валюты")
+        return self.balance == other.balance
+
+    def __add__(self, other):
+        if isinstance(other, Wallet) and self.currency == other.currency:
+            return Wallet(self.currency, self.balance + other.balance)
+        else:
+            raise ValueError("Данная операция запрещена")
+
+    def __sub__(self, other):
+        if isinstance(other, Wallet) and self.currency == other.currency:
+            return Wallet(self.currency, self.balance - other.balance)
+        else:
+            raise ValueError("Данная операция запрещена")
